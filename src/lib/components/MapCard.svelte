aa53c07ad265d048180be5e8947d3a96f030a86e
Pear <20259871+TheRealPear@users.noreply.github.com>
2026-05-14 00:45:28 -0400

    Low effort individual map page

--- src/lib/components/MapCard.svelte
+++ src/lib/components/MapCard.svelte
@@ -11,13 +11,15 @@
 </style>
 
 <!-- each card size is 288x244.3 -->
+<a href="/maps/{map._id}">
+  <div class="card shadow image-full relative overflow-hidden">
 
-<div class="card shadow image-full relative overflow-hidden">
-  <figure>
-    <img class="object-cover" src={`https://raw.githubusercontent.com/TheRealPear/MapScraper/refs/heads/main/Maps/${map.name}/map.png`} alt="" loading="lazy" fetchpriority="low" decoding="async" on:error={(e) => { const img = e.target as HTMLImageElement; img.onerror = null; img.src = '/img/map-fallback.png'; }}/>
-  </figure>
-  <div class="absolute inset-x-0 top-0 h-2/5 bg-gradient-to-b from-black/70 to-transparent pointer-events-none"><!-- dummy div to add background to make card-body readable --></div>
-  <div class="card-body relative">
-    <h2 class="card-title text-[#fff] text-xl font-bold">{map.name}</h2>
+      <figure>
+        <img class="object-cover" src={`https://raw.githubusercontent.com/TheRealPear/MapScraper/refs/heads/main/Maps/${map.name}/map.png`} alt="" loading="lazy" fetchpriority="low" decoding="async" on:error={(e) => { const img = e.target as HTMLImageElement; img.onerror = null; img.src = '/img/map-fallback.png'; }}/>
+      </figure>
+      <div class="absolute inset-x-0 top-0 h-2/5 bg-gradient-to-b from-black/70 to-transparent pointer-events-none"><!-- dummy div to add background to make card-body readable --></div>
+      <div class="card-body relative">
+        <h2 class="card-title text-[#fff] text-xl font-bold">{map.name}</h2>
+      </div>
   </div>
-</div>
+</a>
---
+++ src/routes/maps/[mapid]/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+    import Metadata from '$lib/components/Metadata.svelte';
+    export let data: PageData;
+
+    const map = (data.map ?? {}) as any;
+
+    // Map details
+    const mapName = map.name ?? 'Unknown';
+    const mostRecentMatch = map.lastMatchId ?? null;
+
+
+</script>
+
+<Metadata title={mapName}/>
+
+<h1>{mapName}</h1>
+<p class="prose"><a href="/m/{mostRecentMatch}">Last match on this map</a></p>
